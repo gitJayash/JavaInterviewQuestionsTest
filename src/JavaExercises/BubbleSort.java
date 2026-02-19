b77b1b66320e2c494ec3f9d8dacc514a0705243e
gitJayash <jayashsandaruwan@gmail.com>
2026-02-19 23:14:28 +0530

    first commit

---
+++ src/JavaExercises/BubbleSort.java
@@ -0,0 +1,17 @@
+package JavaExercises;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main (String[]args) {
+
+        int[] a = {5,1,4,2,8};
+        int n = a.length;
+
+        System.out.println("Before Sorting : " + Arrays.toString(a));
+
+        
+
+    }
+}
