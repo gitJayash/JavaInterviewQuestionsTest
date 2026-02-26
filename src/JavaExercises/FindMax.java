28a98d1ce466dc15e69b87a019514b76a7e728f3
gitJayash <jayashsandaruwan@gmail.com>
2026-02-26 10:14:17 +0530

    first commit of fm2

--- src/JavaExercises/FindMax.java
+++ src/JavaExercises/FindMax.java
@@ -1,4 +1,21 @@
 package JavaExercises;
 
 public class FindMax {
+
+    public static void main(String[]args){
+
+    int[] numbers = {23, 45, 71, 99, 2, 12};
+
+    int max = numbers[0];
+
+    for(int num:numbers) {
+        if (num > max) {
+            max = num;
+            }
+        }
+    System.out.println("The highest value is " + max);
+
+    }
 }
+
+
